ced16825846ebf8bd152e76bb15cd660411fd574
joe_jing <joe_jing_91@163.com>
2022-08-08 11:14:16 +0800

    Update MemLock.py

--- MemLock.py
+++ MemLock.py
@@ -1,30 +1,51 @@
-import memcache
+"""
+    memcached_lock
+    ~~~~~~~~~~~~~~
+    Implements a distributed transaction using memcached or
+    a memcached compatible storage.
+    Example::
+        from __future__ import with_statement
+        import memcache
+        from memcached_lock import dist_lock
+        client = memcache.Client(['127.0.0.1:11211'])
+        with dist_lock('test', client):
+            print 'Is there anybody out there!?'
+    :modify from amix the lucky stiff.
+    :license: BSD
+"""
+
+from __future__ import with_statement
+
 import time
 
+from contextlib import contextmanager
+from random import random
+import memcache
 
-class MemLock:
+DEFAULT_EXPIRES = 15
+DEFAULT_RETRIES = 5
 
-    def __init__(self, exp=10):
-        self.client = memcache.Client(['127.0.0.1:11211'], debug=0)
-        self.time = {}
-        self.exp = int(exp)
+client = memcache.Client(['127.0.0.1:11211'], debug=0)
 
-    def __del__(self):
-        self.client.disconnect_all()
+@contextmanager
+def dist_lock(key):
+    key = '__d_lock_%s' % key
 
-    def acquire_lock(self, key):
-        while True:
-            value = self.client.get(key)
-            if value is None:
-                # not find lock key
-                if 0 != self.client.add(key, f'lock_{key}', time=self.exp):
-                    # Nonzero on success, get the lock
-                    self.time[key] = int(time.time())
-                    break
-            time.sleep(0.001)
+    try:
+        _acquire_lock(key, client)
+        yield
+    finally:
+        _release_lock(key, client)
 
-    def release_lock(self, key):
-        if int(time.time()) - self.time[key] < (self.exp - 1):
-            self.time.pop(key)
-            self.client.delete(key)
-            
+def _acquire_lock(key, client):
+    for i in range(0, DEFAULT_RETRIES):
+        stored = client.add(key, 1, DEFAULT_EXPIRES)
+        if stored:
+            return
+        sleep_time = (((i+1)*random()) + 2**i) / 2.5
+        print('Sleeipng for %s' % (sleep_time))
+        time.sleep(sleep_time)
+    raise Exception('Could not acquire lock for %s' % key)
+
+def _release_lock(key, client):
+    client.delete(key)
